65f44ea3b251b5cbd9307aaf6e73bba07d00299a
Fé Gorissen <fe.gorissen@outlook.com>
2025-05-26 18:40:08 +0200

    room opstelling, room beschrijving toegevoegd

--- dungeon_cr.c
+++ dungeon_cr.c
@@ -180,3 +180,132 @@ bool is_already_connected(Room* room1, Room* room2) {
     }
     return false;
 }
+Dungeon* generate_dungeon(int num_rooms) {
+    if (num_rooms <= 0) return NULL;
+
+    Dungeon* dungeon = (Dungeon*)malloc(sizeof(Dungeon));
+    dungeon->num_rooms = num_rooms;
+    dungeon->rooms = (Room**)malloc(num_rooms * sizeof(Room*));
+
+    // Maak kamers met 1-4 deuren
+    for (int i = 0; i < num_rooms; i++) {
+        int max_doors = 1 + rand() % 4;
+        dungeon->rooms[i] = create_room(i, max_doors);
+    }
+
+    dungeon->entrance = dungeon->rooms[0];
+    
+    // Initialiseer speler
+    dungeon->player.current_room_id = 0;
+    dungeon->player.hp = 100;
+    dungeon->player.max_hp = 100;
+    dungeon->player.damage = 10 + rand() % 10;
+    dungeon->player.has_treasure = false;
+
+    // Verbind kamers in een boomstructuur
+    for (int i = 1; i < num_rooms; i++) {
+        int parent_idx = rand() % i;
+        connect_rooms(dungeon->rooms[parent_idx], dungeon->rooms[i]);
+    }
+
+    // Voeg extra verbindingen toe
+    for (int i = 0; i < num_rooms; i++) {
+        Room* current = dungeon->rooms[i];
+        int remaining_doors = current->max_doors - current->num_doors;
+
+        for (int j = 0; j < remaining_doors; j++) {
+            int target_idx;
+            int attempts = 0;
+            do {
+                target_idx = rand() % num_rooms;
+                attempts++;
+            } while ((target_idx == i || is_already_connected(current, dungeon->rooms[target_idx])) && attempts < 100);
+
+            if (attempts < 100) {
+                connect_rooms(current, dungeon->rooms[target_idx]);
+            }
+        }
+    }
+
+    return dungeon;
+}
+
+void populate_rooms(Dungeon* dungeon) {
+    // Eerst alle kamers resetten (behalve startkamer)
+    for (int i = 1; i < dungeon->num_rooms; i++) {
+        dungeon->rooms[i]->content.type = EMPTY;
+        dungeon->rooms[i]->cleared = false;
+    }
+
+    // Plaats de schat in een willekeurige kamer (niet startkamer)
+    int treasure_room = 1 + rand() % (dungeon->num_rooms - 1);
+    dungeon->rooms[treasure_room]->content.type = TREASURE;
+    dungeon->rooms[treasure_room]->cleared = false;
+
+    // Plaats minstens 1 monster in een andere kamer
+    int monster_room;
+    do {
+        monster_room = 1 + rand() % (dungeon->num_rooms - 1);
+    } while (monster_room == treasure_room);
+    
+    dungeon->rooms[monster_room]->content.type = MONSTER;
+    dungeon->rooms[monster_room]->content.content.monster = create_monster(rand() % MAX_MONSTER_TYPES);
+    dungeon->rooms[monster_room]->cleared = false;
+
+    // Vul de rest van de kamers random in
+    for (int i = 1; i < dungeon->num_rooms; i++) {
+        if (i == treasure_room || i == monster_room) continue;
+
+        int rand_val = rand() % 100;
+        if (rand_val < 40) {  // 40% kans op monster
+            dungeon->rooms[i]->content.type = MONSTER;
+            dungeon->rooms[i]->content.content.monster = create_monster(rand() % MAX_MONSTER_TYPES);
+        } 
+        else if (rand_val < 75) {  // 35% kans op item
+            dungeon->rooms[i]->content.type = ITEM;
+            dungeon->rooms[i]->content.content.item = create_item();
+        }
+        // 25% kans blijft leeg
+    }
+}
+
+void print_room_description(Room* room) {
+    printf("\n=== Kamer %d ===\n", room->id);
+    
+    if (!room->visited) {
+        printf("Je betreedt deze kamer voor het eerst.\n");
+        room->visited = true;
+    }
+
+    switch(room->content.type) {
+        case MONSTER:
+            if (!room->cleared) {
+                printf("Er staat een %s klaar om aan te vallen! (HP: %d, Damage: %d)\n", 
+                       room->content.content.monster->name,
+                       room->content.content.monster->hp,
+                       room->content.content.monster->damage);
+            } else {
+                printf("Het dode lichaam van een %s ligt hier.\n",
+                       room->content.content.monster->name);
+            }
+            break;
+        case ITEM:
+            if (!room->cleared) {
+                printf("Je ziet een %s op de grond.\n",
+                       room->content.content.item->name);
+            } else {
+                printf("Deze kamer is leeg.\n");
+            }
+            break;
+        case TREASURE:
+            if (!room->cleared) {
+                printf("De schat ligt hier glinsterend in het midden van de kamer!\n");
+            } else {
+                printf("De schatkist staat hier open en leeg.\n");
+            }
+            break;
+        default:
+            printf("Deze kamer is leeg.\n");
+    }
+}
+
